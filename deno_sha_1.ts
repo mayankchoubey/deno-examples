1c7ed44da8eb281da423bf89605a56b3f2cc330f
mayankchoubey <choubey@gmail.com>
2021-06-12 12:20:34 -0700

    Calculate SHA-1

---
+++ deno_sha_1.ts
@@ -0,0 +1,9 @@
+import { encodeToString } from "https://deno.land/std/encoding/hex.ts";//common input for all functions
+
+const i=Uint8Array.from({length: 50}, () => Math.floor(Math.random() * 50));//SHA-1
+
+console.log(new Uint8Array(await crypto.subtle.digest('sha-1', i)));
+//Uint8Array(20) [87, 55, 240, 228, 250, 122, 3, 204, 85, 72, 125,  85, 159, 198, 81, 239, 35, 254, 77, 27]
+
+console.log(encodeToString(new Uint8Array(await crypto.subtle.digest('sha-1', i))));
+//df3ed451c9ea86ab10ff7583874ad1bd4813c3e0
