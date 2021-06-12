67ee58e1bf642e1ad9fd17b7146d3a9b27cdc49a
mayankchoubey <choubey@gmail.com>
2021-06-12 12:27:09 -0700

    Generate V4 UUID

---
+++ deno_v4_uuid.ts
@@ -0,0 +1,8 @@
+for(let i=0; i<5; i++)
+    console.log(crypto.randomUUID());
+
+//49d25949-7045-4bdd-a17b-a5b4e3f16be1
+//0b661bd4-61a3-4d33-bb39-5d30d299d7b0
+//4ce3bf60-7633-47d1-abce-6a0b83fcae72
+//27d3c909-3c71-4f0a-a683-c16b11fa1e81
+//7f3995df-504d-4bc9-bbf2-3338749ab610
